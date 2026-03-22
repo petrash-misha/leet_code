91f7ac20f9c44151c07b135e8e8bedc2dbf83e37
petrash-misha <petrash.mihail@gmail.com>
2026-03-22 17:49:36 +0000

    Added new Tree Project - 112.Path sum

--- Tree/path_sum.py
+++ Tree/path_sum.py
@@ -61,3 +61,15 @@ class Solution:
 
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         return self.DFS(root, 0, targetSum)
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+
+        if not root.left and not root.right:
+            return targetSum - root.val == 0
+
+        targetSum -= root.val
+
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
